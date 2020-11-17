97b1c0765a775a5a1a53b7af3e077d04dfff47bb
swwgl <weiweigl.outlook.com>
2020-11-17 19:03:54 +0800

    添加登录注册后的提交

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const route = require("./route");
+const session = require("express-session");
 const app = express();
 
 app.listen(4000);
@@ -7,6 +8,16 @@ app.listen(4000);
 app.set("view engine", "ejs");
 
 app.use(express.static("./public"));
+// 获取post 提交的参数
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "message",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.get("/", function (req, res) {
   // res.render("index");
@@ -16,3 +27,10 @@ app.get("/", function (req, res) {
 
 // 处理message相关的请求(所有以/message开头的请求)
 app.use("/message", route.message);
+
+// 处理user相关的请求(以/user开头的请求)
+app.use("/user", route.user);
+// 其他的 错误地址
+app.use(function (req, res) {
+  res.render("error", { msg: "请求地址错误" });
+});
--- route/user.js
+++ route/user.js
@@ -1,5 +1,42 @@
 const router = require("express").Router();
+const db = require("../model/mydb");
+const User = db.User;
 
+// get 的 /user/login请求,跳转到登录页面
+router.get("/login", function (req, res) {
+  res.render("login");
+});
+// get 的 /user/regist请求,跳转到注册页面
+router.get("/regist", function (req, res) {
+  res.render("regist");
+});
 
+// post 的 /user/login请求,处理登录
+router.post("/login", function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  // 判断用户名密码是否正确
+  var filter = {
+    username: username,
+    password: password,
+  };
+  db.findAll(User, filter, function (err, users) {
+    if (err) {
+      console.log(err);
+      res.send({ status: 1, msg: "网络波动,稍后继续..." });
+      return;
+    }
+    if (users.length == 0) {
+      // 没有找到数据,用户名或密码错误
+      res.send({ status: 1, msg: "用户名或密码错误" });
+      return;
+    }
+    // 登录成功
+    // 保存登录状态
+    req.session.username = username;
+    // 返回成功的状态
+    res.send({ status: 0, msg: "登录成功" });
+  });
+});
 
 module.exports = router;
